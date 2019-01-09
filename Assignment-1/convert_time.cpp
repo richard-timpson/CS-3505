6d57cd880768b4374f00688063dbe774843a92ef
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2019-01-09 12:47:14 -0700

    working on first program

---
+++ Assignment-1/convert_time.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+  double seconds_decimal;
+  cout << "Enter a time in seconds: ";
+  cin >> seconds;
+  int seconds = round(seconds_decimal);
+  cout << round(seconds_decimal) << " seconds \n";
+}
