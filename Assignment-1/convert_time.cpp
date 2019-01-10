0eea64a55e00696a7484f2b675b43f0af1a9acfb
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2019-01-10 10:25:00 -0700

    almost have the first assignment finished

--- Assignment-1/convert_time.cpp
+++ Assignment-1/convert_time.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main () 
 {
   double seconds_decimal;
   cout << "Enter a time in seconds: ";
-  cin >> seconds;
-  int seconds = round(seconds_decimal);
-  cout << round(seconds_decimal) << " seconds \n";
+  cin >> seconds_decimal;
+  int result = round(seconds_decimal);
+  int hours = result / 3600;
+  int minutes = ((result % 3600) * 60) / 60;
+  
+  cout << result  << " seconds\n";
+  cout << hours << "hours\n";
+  cout << minutes << "minutes \n";
+  // convert seconds to hours. 
+  // take the remainder of hours and convert to minutes
+  // take the remainder of minutes and convert to hours. 
 }
