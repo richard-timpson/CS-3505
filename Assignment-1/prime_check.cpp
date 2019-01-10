c0039396a0b354f7e216105c491f7b79ce087105
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2019-01-10 16:36:22 -0700

    have the prime number working, need to comment and finish up both programs

--- Assignment-1/prime_check.cpp
+++ Assignment-1/prime_check.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
 #include <cmath>
-
-int main ()
+#include<cstdlib>
+using namespace std;
+int main (int argc, char** argv)
+{
+  int argument = atoi( argv[1]);
+  bool isPrime;
+  if (argument <= 1){
+    isPrime = true;
+  }
+  else {
+  isPrime = true;
+     for (int i = 2; i < argument; i++) {
+       if (argument%i == 0) {
+	 isPrime = false;
+       }
+     }
+  }
+  if (isPrime) {
+    cout << "prime" << endl;
+  }
+  else {
+    cout << "not prime" << endl;
+  }
+  
+}
