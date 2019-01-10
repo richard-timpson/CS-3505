81b7d7b3673a21fd256e89c5c641313c6f05bc0f
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2019-01-10 16:45:16 -0700

    added documentation for assignment

--- Assignment-1/convert_time.cpp
+++ Assignment-1/convert_time.cpp
@@ -1,17 +1,39 @@
+/*
+CS 3505
+Richard Timpson
+1/10/19
+Convert Time Program
+
+The program will ask the user to input a number in seconds, and output those seconds
+as hours, minutes, and seconds. 
+*/
+
+
+// including relevant libraries
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main () 
 {
+  // storing the user input as a double in the case of a decimal
   double seconds_decimal;
   cout << "Enter a time in seconds: ";
   cin >> seconds_decimal;
+
+  // rounding the number and storing as an integer
   int seconds_rounded = round(seconds_decimal);
+
+  // performing calculations for each aspect of the time
   int hours = seconds_rounded / 3600;
   int minutes = ((seconds_rounded % 3600) / 60);
   int seconds = ((seconds_rounded % 3600))% 60; 
+
+  // outputting the seconds
   cout << seconds_rounded << " seconds --> ";
+  
+  // the following checks check if the hours, minutes, or seconds are above 10
+  // if they are, print a 0 before the number so that we are using the proper format. 
   if (hours < 10 ) {
     cout << "0";
   }
@@ -23,8 +45,6 @@ int main ()
   if (seconds < 10){
     cout << "0";
   }
-  cout << seconds << "\n";
-  // convert seconds to hours. 
-  // take the remainder of hours and convert to minutes
-  // take the remainder of minutes and convert to hours. 
+  // finish the output. 
+  cout << seconds << "\n"; 
 }
--- Assignment-1/prime_check.cpp
+++ Assignment-1/prime_check.cpp
@@ -1,22 +1,44 @@
+/*
+CS 3505
+Richard Timpson
+1/10/19
+
+Program should take in one input, and will output to the console whether or not that number is a prime nubmer
+
+ */
+
+// including relevant libraries
 #include <iostream>
 #include <cmath>
 #include<cstdlib>
+
 using namespace std;
+
 int main (int argc, char** argv)
 {
+  // storing the argument as an integer
   int argument = atoi( argv[1]);
+
+  // setting a flag that tells us whether or not the number is prime
   bool isPrime;
+  
+  // if the argument is 1 or less, it is considered a prime number, set to true
   if (argument <= 1){
     isPrime = true;
   }
   else {
+    // if it isn't, loop through every possible number that could divide the argument
+    // if one of those arguments has a clean division, with no remainder, the number
+    // is not prime. 
   isPrime = true;
      for (int i = 2; i < argument; i++) {
        if (argument%i == 0) {
 	 isPrime = false;
        }
      }
+     // if we reach the end of the loop and the number is prime, the flag will be true
   }
+  // perform simple check to determine the output. 
   if (isPrime) {
     cout << "prime" << endl;
   }
