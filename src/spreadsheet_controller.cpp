b6e0f8581c0e0ebabf84c46fbfc5fa2bb9ffa042
Connor Grimes <u1123235@lab1-19.eng.utah.edu>
2019-04-03 16:29:48 -0600

    Added ValidateUser pseudo to spreadsheet controller

---
+++ src/spreadsheet_controller.cpp
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Validate User
+ * 
+ * if (UserList.include(username) // UserList is a .txt file loaded in when the server is started.
+ * {
+ * check password
+ * if good, good
+ * if bad, send error and break.
+ * }
+ * else
+ * {
+ * UserModel::makeNewUser();
+ * }
+ * 
+ * Send sheet to user
+ *
+ */
