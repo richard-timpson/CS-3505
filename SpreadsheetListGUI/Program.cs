7d46e5280c3005264b5ab5b88dffb7927be431aa
Peter Forsling <peterforsling@users.noreply.github.com>
2019-04-03 10:03:58 -0600

    Added some buttons and comments for methods to be developed in the SpreadsheetSuiteGUI

--- SpreadsheetListGUI/Program.cs
+++ SpreadsheetListGUI/Program.cs
@@ -16,7 +16,7 @@ namespace SpreadsheetListGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SpreadsheetSuiteGUI());
         }
     }
 }
