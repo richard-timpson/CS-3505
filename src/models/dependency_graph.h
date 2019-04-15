2405cf3ec07ca6916c68ea046ddbdb72f4187f09
Connor Grimes <u1123235@lab1-20.eng.utah.edu>
2019-04-15 17:40:13 -0600

    Finished main work on dependency graph

--- src/models/dependency_graph.h
+++ src/models/dependency_graph.h
@@ -20,19 +20,20 @@ namespace Backend
     int get_size_of_dependees(std::string input);
     bool has_dependents(std::string input);
     bool has_dependees(std::string input);
-    void get_dependents(std::string input);
-    void get_dependees(std::string input);
+    std::unordered_set<std::string>::const_iterator get_dependents(std::string input);
+    std::unordered_set<std::string>::const_iterator get_dependees(std::string input);
     void add_dependency(std::string first, std::string second);
     void remove_dependency(std::string first, std::string second);
     // Replace dependents and dependees methods go here.
     
     
-    
+    std::unordered_map<std::string, std::unordered_set<std::string>> dependees;
+
+    std::unordered_map<std::string, std::unordered_set<std::string>> dependents;
+
+    int num_pairs;
     
   private:
-    std::unordered_map<std::string, std::unordered_set<std::string>> depends_on_graph;
-    std::unordered_map<std::string, std::unordered_set<std::string>> depended_on_by_graph;
-    int num_pairs;
     
     
   };
