6b0f449ff66f0a2a4919d71ecf2fa80c61b3c476
Richard Timpson <richardtimpson80@gmail.com>
2019-04-18 12:27:51 -0600

    latest

--- src/models/SpreadsheetModel.cpp
+++ src/models/SpreadsheetModel.cpp
@@ -146,7 +146,7 @@ std::string SpreadsheetModel::get_cell_contents(std::string name)
     }
 }
 
-std::vector<std::string> SpreadsheetModel::get_cell_direct_dependents(std::string name)
+std::vector<std::string> SpreadsheetModel::get_direct_dependents(std::string name)
 {
     std::unordered_map<std::string, Cell>::iterator it = cell_dictionary.find(name);
     if (it != cell_dictionary.end())
